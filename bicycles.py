1e49d092a2e8a09cb9e629c1fd2b05be1c41b5d9
Rashmita uppalli <rashmita@Rashmitas-MacBook-Pro.local>
2025-08-04 14:34:30 -0400

    bicycle program

--- bicycles.py
+++ bicycles.py
@@ -1,7 +1,7 @@
 bicycles=['trek','cannondale','redline','specialized']
 print(bicycles)
 
-bicycles=['trek','cannondale','redline','specialized']
+bicycles=['trek','cannondale','redline','specialized']# Accessing elements in a list 
 print(bicycles[0])
 
 bicycles=['trek','cannondale','redline','specialized']
