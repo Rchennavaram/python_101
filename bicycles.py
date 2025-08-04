43896cb79637dadc9a4108cde0165277551469ad
Rashmita uppalli <rashmita@Rashmitas-MacBook-Pro.local>
2025-08-04 12:38:23 -0400

    program

---
+++ bicycles.py
@@ -0,0 +1,13 @@
+bicycles=['trek','cannondale','redline','specialized']
+print(bicycles)
+
+bicycles=['trek','cannondale','redline','specialized']
+print(bicycles[0])
+
+bicycles=['trek','cannondale','redline','specialized']
+print(bicycles[0].title())
+
+
+bicycles=['trek','cannondale','redline','specialized']
+print(bicycles[1])
+print(bicycles[3])
