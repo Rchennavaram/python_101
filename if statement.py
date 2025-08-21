d5b07ebecf53854b86e7fdc5f44d9823acabac38
Rashmita uppalli <rashmita@Rashmitas-MacBook-Pro.local>
2025-08-21 13:41:41 -0400

    if statement program

---
+++ if statement.py
@@ -0,0 +1,14 @@
+trains = ['audi','bmw','subaru','toyota']
+for train in trains:
+    if train == 'subaru':
+        print(train.upper())
+    else:
+        print(train.title())
+
+a = 'mushrooms'
+if a !='anchovies':
+    print("hold the anchovies!")
+
+answer=17
+if answer != 27:
+   print("This answer is not correct, Try again later.")
